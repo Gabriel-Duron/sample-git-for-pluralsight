e3bfe6ad56d3c284055528bbda1e987d42ada693
Gabriel <Gabriel.gida@gmail.com>
2022-07-13 15:12:14 -0500

    Update the toString Method

--- src/com/gabriel/model/User.java
+++ src/com/gabriel/model/User.java
@@ -12,11 +12,6 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	@Override
-	public String toString() {
-		return "User [name=" + name + "]";
-	}
 	
 	public int getAge() {
 		return age;
@@ -24,6 +19,11 @@ public class User {
 
 	public void setAge(int age) {
 		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + "]";
 	}
 	
 }
